5ce930642da6809262eb92ffc4516b45e2ebe022
Laurent LAPORTE <tantale.solutions@gmail.com>
2017-07-10 21:17:33 +0200

    Fix spelling in documentation.

--- deprecated/__init__.py
+++ deprecated/__init__.py
@@ -7,7 +7,7 @@ import warnings
 def deprecated(func):
     """
     This is a decorator which can be used to mark functions
-    as deprecated. It will result in a warning being emmitted
+    as deprecated. It will result in a warning being emitted
     when the function is used.
     """
 
