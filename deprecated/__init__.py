def27eba6874ae5574d4338beef8f7f8219d3753
dschwoerer <dschwoerer@users.noreply.github.com>
2021-10-11 14:18:57 +0200

    use context manager for changing warnings
    This ensures the outside scope is not changed.

--- deprecated/__init__.py
+++ deprecated/__init__.py
@@ -86,13 +86,12 @@ def deprecated(reason):
 
             @functools.wraps(func1)
             def new_func1(*args, **kwargs):
-                warnings.simplefilter('always', DeprecationWarning)
-                warnings.warn(
-                    fmt1.format(name=func1.__name__, reason=reason),
-                    category=DeprecationWarning,
-                    stacklevel=2
-                )
-                warnings.simplefilter('default', DeprecationWarning)
+                with warnings.simplefilter('always', DeprecationWarning):
+                    warnings.warn(
+                        fmt1.format(name=func1.__name__, reason=reason),
+                        category=DeprecationWarning,
+                        stacklevel=2
+                    )
                 return func1(*args, **kwargs)
 
             return new_func1
@@ -118,13 +117,12 @@ def deprecated(reason):
 
         @functools.wraps(func2)
         def new_func2(*args, **kwargs):
-            warnings.simplefilter('always', DeprecationWarning)
-            warnings.warn(
-                fmt2.format(name=func2.__name__),
-                category=DeprecationWarning,
-                stacklevel=2
-            )
-            warnings.simplefilter('default', DeprecationWarning)
+            with warnings.simplefilter('always', DeprecationWarning):
+                warnings.warn(
+                   fmt2.format(name=func2.__name__),
+                    category=DeprecationWarning,
+                    stacklevel=2
+                )
             return func2(*args, **kwargs)
 
         return new_func2
